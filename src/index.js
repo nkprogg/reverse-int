f1d2deabb7356c6ae9d7dc8497bd8d2aeb5b8feb
Natallia <natallia.kapitan@gmail.com>
2023-08-12 19:38:17 +0300

    init: complite task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+      let string = String(Math.abs(n)) ;
+      let result = string.split("").reverse().join("");
+      
+      return Number(result);
 }
